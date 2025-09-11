379ae909af2a231d5a92be9ad25db8a85eebb864
mech1taz <ma.astudillom@duocuc.cl>
2025-09-11 15:43:31 -0300

    s

--- js/productos.js
+++ js/productos.js
@@ -1,30 +1,34 @@
 // Lista de productos de Objetivo Café
 const productos = [
+  //id de los cafés comienzan desde el 1 al 100
   {
     id: 1,
     nombre: "Café de Colombia 250g",
-    descripcion: "Café en grano de origen colombiano, balanceado y aromático.",
-    precio: 6000,
+    descripcion: " ",
+    precio: 10990,
     tipo: "cafe",
     opciones: ["Grano", "Molido - Italiana", "Molido - Prensa Francesa", "Molido - Filtro"],
     imagen: "img/cafe-colombia.png"
   },
+  
   {
     id: 2,
+    nombre: "Café Etiopia 250g",
+    descripcion: "Café en grano de origen colombiano, balanceado y aromático.",
+    precio: 10990,
+    tipo: "cafe",
+    opciones: ["Grano", "Molido - Italiana", "Molido - Prensa Francesa", "Molido - Filtro"],
+    imagen: "img/cafe_etiopia.png"
+  },
+  //id de las cafeteras desde el 101 al 200
+  {
+    id: 101,
     nombre: "Cafetera Francesa 600ml",
     descripcion: "Vidrio borosilicato con filtro de acero inoxidable.",
-    precio: 15000,
-    tipo: "accesorio",
+    precio: 12990,
+    tipo: "cafetera",
     opciones: [], // no necesita molienda
     imagen: "img/cafetera-francesa.png"
   },
-  {
-    id: 3,
-    nombre: "Cafetera Italiana 350ml",
-    descripcion: "Clásica cafetera moka de aluminio.",
-    precio: 12000,
-    tipo: "accesorio",
-    opciones: [],
-    imagen: "img/cafetera-italiana.png"
-  }
+  //id de los accesorios desde el 201 al 300
 ];
